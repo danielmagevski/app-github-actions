8abf4e98756b08d2b942b8b61f8ca15411520185
Daniel Magevski <daniel_magevski@hotmail.com>
2023-11-05 19:11:56 -0300

    update

--- terraform/route-53.tf
+++ terraform/route-53.tf
@@ -1,8 +1,7 @@
-/*resource "aws_route53_record" "this" {
-  name    = "app"
-  type    = "CNAME"
-  zone_id = "Z03401263USG3HK0BZ0JZ"
-
-  records = [aws_lb.this.dns_name]
-  ttl     = "300"
-}*\
+#resource "aws_route53_record" "this" {
+#  name    = "app"
+#  type    = "CNAME"
+#  zone_id = "Z03401263USG3HK0BZ0JZ"
+# records = [aws_lb.this.dns_name]
+ # ttl     = "300"
+#}
