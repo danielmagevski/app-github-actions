10c406e44703f814fdfe984a3c954c7bd07eddac
Daniel Magevski <daniel_magevski@hotmail.com>
2023-11-05 15:27:02 -0300

    update

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,24 +3,22 @@ resource "aws_instance" "node1" {
   instance_type               = "t2.medium"
   subnet_id                   = aws_subnet.public_a.id
   key_name                    = "test-lab"
-  security_groups             = [aws_security_group.swarm.id]
+  vpc_security_group_ids      = [aws_security_group.swarm.id]
   associate_public_ip_address = true
   tags = {
     Name = "ec2_instance_az-a"
   }
-  depends_on = [aws_security_group.swarm]
 }
 resource "aws_instance" "node2" {
   ami                         = "ami-05c13eab67c5d8861"
   instance_type               = "t2.medium"
   subnet_id                   = aws_subnet.public_b.id
   key_name                    = "test-lab"
-  security_groups             = [aws_security_group.swarm.id]
+  vpc_security_group_ids      = [aws_security_group.swarm.id]
   associate_public_ip_address = true
 
   tags = {
     Name = "ec2_instance_az-b"
   }
-  depends_on = [aws_security_group.swarm]
 }
 
