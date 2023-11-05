85d93122de2017b53ceda218604682bd7b8f1f11
Daniel Magevski <daniel_magevski@hotmail.com>
2023-11-05 19:03:59 -0300

    update

--- terraform/route-53.tf
+++ terraform/route-53.tf
@@ -1,8 +1,8 @@
-resource "aws_route53_record" "this" {
-  name    = "swarm-lb-dns-cname"
+/*resource "aws_route53_record" "this" {
+  name    = "app"
   type    = "CNAME"
-  zone_id = "Z03401263USG3HK0BZ0JZ" # Substitua pelo ID da zona DNS
+  zone_id = "Z03401263USG3HK0BZ0JZ"
 
   records = [aws_lb.this.dns_name]
   ttl     = "300"
-}
+}*\
