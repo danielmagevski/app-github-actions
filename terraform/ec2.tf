804ac6d5b00e2e2e918e0bc509e1aae8fb9aee47
Daniel Magevski <daniel_magevski@hotmail.com>
2023-11-05 14:12:31 -0300

    fix-format

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "node1" {
   instance_type               = "t2.medium"
   subnet_id                   = aws_subnet.public_a.id
   key_name                    = "test-lab"
-  security_groups = [ aws_security_group.swarm.id ]
+  security_groups             = [aws_security_group.swarm.id]
   associate_public_ip_address = true
 
   user_data = <<-EOF
@@ -14,19 +14,19 @@ resource "aws_instance" "node1" {
   tags = {
     Name = "ec2_instance_az-a"
   }
-  depends_on = [ aws_security_group.swarm ]
+  depends_on = [aws_security_group.swarm]
 }
 resource "aws_instance" "node2" {
   ami                         = "ami-05c13eab67c5d8861"
   instance_type               = "t2.medium"
   subnet_id                   = aws_subnet.public_b.id
   key_name                    = "test-lab"
-  security_groups = [ aws_security_group.swarm.id ]
+  security_groups             = [aws_security_group.swarm.id]
   associate_public_ip_address = true
 
   tags = {
     Name = "ec2_instance_az-b"
   }
-  depends_on = [ aws_security_group.swarm ]
+  depends_on = [aws_security_group.swarm]
 }
 
